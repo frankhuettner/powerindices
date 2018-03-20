2116381f2e601e7ca5d1fdc287740829fb793be5
frankhuettner <36417105+frankhuettner@users.noreply.github.com>
2018-03-21 00:56:42 +0100

    all fixed. looks good as is

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ the Shapley Shubik index (SSI), and
 the Coleman Shapley index (CSI)
 for weighted voting games. 
 Both, quota and weights must be **integers**.
-
+Moreover, it is possible to give an optional arguemnent: the minimal size of a winning coalition.
 
 For information about the indices:
 
@@ -14,6 +14,13 @@ PBI: https://en.wikipedia.org/wiki/Banzhaf_power_index
 SSI: https://en.wikipedia.org/wiki/Shapley%E2%80%93Shubik_power_index
 
 CSI: a paper soon available by Andre Casajus and Frank Huettner
+
+
+## Examples
+The example.py offers some examples, e.g. the powerindices for the UN Security Council. To this end, the quota 39 and the weights are [7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], where the veto powers have 7 and the nonpermanent members have weight 1. 
+
+Also, the powerindices for the EU Council is computable: the quota is 65% of the population and the weight of every country is the population that lives in this country. Moreover, the minimal size of a winning coalition must be specified: setting minimalWinningCoalitionSize=16 ensures that only coalitions with at least 16 members (i.e., 55% of the countries) are winning.
+
 
 ## Installation
 No installation. Just copy powerindices.py to where you need it. Run the exmaple.py file in the same directory if you want to test it.
--- example.py
+++ example.py
@@ -6,37 +6,70 @@ CORRECT: quota = 51, weigths = [25, 14, 33, 11, 17]
 '''
 
 # This example represents the UN security council.
-# quota,weights = 39, [7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
-quota,weights = 8,[4,4,3,2,1,1]   # a smaller example
-# quota,weights = 8,[4,0,3,2,1,1]   # a smaller example with a null player
-
+quota,weights = 39, [7, 7, 7, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
 # call the functions to compute the respective index
-# PBIs = powerindices.compute_pbi(quota,weights, minimalWinningCoalitionSize=1)
-SSIs = powerindices.compute_ssi(quota,weights, minimalWinningCoalitionSize=1)
-CSIsmin = powerindices.compute_csi(quota,weights, minimalWinningCoalitionSize=3)
+PBIs = powerindices.compute_pbi(quota,weights)
+SSIs = powerindices.compute_ssi(quota,weights)
 CSIs = powerindices.compute_csi(quota,weights)
-## The indices are now stored as lists in the PBIs, SSIs, and CSIs.
-## We could simply print these list:
+### The functions return the indices as lists so that
+### they are now stored as lists in the PBIs, SSIs, and CSIs.
+### We could simply print these list:
 # print(SSIs)  
-## but I've prepared a formated output below.
+### but I've prepared a formated output below.
 
 
-# Formated output
+### a simple majority game example with three players
+# PBIs = powerindices.compute_pbi(2,[1,1,1])
+# SSIs = powerindices.compute_ssi(2,[1,1,1])
+# CSIs = powerindices.compute_csi(2,[1,1,1])
+### the following is equivalent: 
+# PBIs = powerindices.compute_pbi(0,[1,1,1], minimalWinningCoalitionSize=2)
+# SSIs = powerindices.compute_ssi(0,[1,1,1], minimalWinningCoalitionSize=2)
+# CSIs = powerindices.compute_csi(0,[1,1,1], minimalWinningCoalitionSize=2)
+
+
+### a small example with a null player
+# quota,weights = 8,[4,0,3,2,1,1]  
+# PBIs = powerindices.compute_pbi(quota,weights)
+# SSIs = powerindices.compute_ssi(quota,weights)
+# CSIs = powerindices.compute_csi(quota,weights)
+
+
+### EU before Brexit:
+### The weights are the population rounded by 10000 of each country.
+### (It is rounded because otherwise it takes longer).
+### 65% of the total population must be represented.
+### Moreover, at least 55% of the countries must approve, i.e. 16 countries.
+# EU28POP = [332490349,8772865,11351727,7101859,4154213,854802,10578820,5748769,1315635,5503297,66989083,82521653,10768193,9797561,4784383,60589445,1950116,2847904,590667,460297,17081507,37972964,10309573,19644350,5435343,2065895,46528966,9995153,65808573]
+# EU28ROUNDED = [int(round(i/10000)) for i in EU28POP]
+# quota,weights = EU28ROUNDED[0],EU28ROUNDED[1:]
+# PBIs = powerindices.compute_pbi(quota,weights, minimalWinningCoalitionSize=16)
+# SSIs = powerindices.compute_ssi(quota,weights, minimalWinningCoalitionSize=16)
+# CSIs = powerindices.compute_csi(quota,weights, minimalWinningCoalitionSize=16)
+
+
+### EU after Brexit
+# EU28POP = [332490349,8772865,11351727,7101859,4154213,854802,10578820,5748769,1315635,5503297,66989083,82521653,10768193,9797561,4784383,60589445,1950116,2847904,590667,460297,17081507,37972964,10309573,19644350,5435343,2065895,46528966,9995153,65808573]
+# EU28ROUNDED = [int(round(i/10000)) for i in EU28POP]
+# quota,weights = EU28ROUNDED[0]-EU28ROUNDED[-1],EU28ROUNDED[1:-1]
+# PBIs = powerindices.compute_pbi(quota,weights, minimalWinningCoalitionSize=15)
+# SSIs = powerindices.compute_ssi(quota,weights, minimalWinningCoalitionSize=15)
+# CSIs = powerindices.compute_csi(quota,weights, minimalWinningCoalitionSize=15)
+
+
+
+
+### Formated output
 numberDigits = 5
-formatspec = '{0:.%df}' % numberDigits
-print('='*(22 + (len(weights)-1)*(numberDigits+5)))
-print('weight of i  || '+' | '.join([formatspec.format(item) for item in weights]))
-print('-'*(22 + (len(weights)-1)*(numberDigits+5)))
-print('SSI of i     || '+' | '.join([formatspec.format(item) for item in SSIs]))
-print('CSImin i     || '+' | '.join([formatspec.format(item) for item in CSIsmin]))
-print('CSI of i     || '+' | '.join([formatspec.format(item) for item in CSIs]))
-# print('PBI of i     || '+' | '.join([formatspec.format(item) for item in PBIs]))
-# print('CSI/PBI of i || '+' | '.join([formatspec.format(item) for item in CSIs]))
-print('='*(22 + (len(weights)-1)*(numberDigits+5)))
-print('(quota: {})'.format(quota))
+formatspec = '{0:.%df}' % (numberDigits)
+print('(quota: {}, sum of CSI/sum of PBI: {:.5f})'.format(quota,sum(CSIs)/sum(PBIs)))
+print('   weight  ||    SSI |     CSI |     PSI |  CSI/PSI \n')
+for i in range(len(SSIs)):
+    print('{:10d} ||'.format(weights[i]) + ' | '.join([formatspec.format(item) for item in [SSIs[i],CSIs[i],PBIs[i],CSIs[i]/PBIs[i] ] ])+'\n')
 
 
-# ##### uncomment the following to get results printed into a file output.txt
+
+#### uncomment the following to get results printed into a file powerindices_output.txt
 # numberDigits = 5   # Set how many digits after comma you want in the output file?
 # formatspec = '{0:.%df}' % numberDigits
 # with open('powerindices_output.txt', 'a') as f:   
@@ -44,4 +77,4 @@ print('(quota: {})'.format(quota))
 #     f.write('  weight ,      SSI ,      CSI ,      PSI ,  CSI/PSI \n')
 #     for i in range(len(SSIs)):
 #         f.write(' , '.join([formatspec.format(item) for item in [weights[i], SSIs[i],CSIs[i],PBIs[i],CSIs[i]/PBIs[i] ] ])+'\n')
-# # #####
+###
